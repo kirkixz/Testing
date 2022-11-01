47acd2c812ae7bc921c232f5a180a42f10a8e41d
inon2re <hulumulunulu@yandex.ru>
2022-11-01 22:59:04 +0300

    Reg Log Menu 60%

--- app/src/main/java/com/example/project3/Log_Activity.kt
+++ app/src/main/java/com/example/project3/Log_Activity.kt
@@ -3,9 +3,9 @@ package com.example.project3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Log_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reg)
+        setContentView(R.layout.activity_log)
     }
 }
